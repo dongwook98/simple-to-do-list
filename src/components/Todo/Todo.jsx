bc80322e900702cd8ae94ab51624e870e1a77d83
강동욱 <ehddnr8563@gmail.com>
2023-12-18 17:59:07 +0900

    add: 리스트 스타일링

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const handleChange = (e) => {
@@ -13,17 +14,22 @@ export default function Todo({ todo, onUpdate, onDelete }) {
 
   const { text, status } = todo;
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type='checkbox'
         id='checkbox'
         checked={status === 'completed'}
         onChange={handleChange}
       />
-      <label htmlFor='checkbox'>{text}</label>
-      <button onClick={handleClick}>
-        <FaTrashAlt />
-      </button>
+      <label htmlFor='checkbox' className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleClick} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </li>
   );
 }
