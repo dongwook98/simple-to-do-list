2f662ae2fdf09861c1510c0c67be5c6ebdb26612
강동욱 <ehddnr8563@gmail.com>
2023-12-18 15:51:12 +0900

    add: 헤더 스타일링

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import styles from './Header.module.css';
 
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {filters.map((value, i) => (
           <li key={i}>
             <button
+              className={`${styles.filter} ${
+                filter === value && styles.selected
+              } `}
               onClick={() => {
                 onFilterChange(value);
               }}
