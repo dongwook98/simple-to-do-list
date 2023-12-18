826c65025056583bc5016138335de395dd65d328
강동욱 <ehddnr8563@gmail.com>
2023-12-18 17:59:44 +0900

    add: 다크모드 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
+import { useDarkMode } from '../../context/DarkModeContext';
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {darkMode ? <MdDarkMode /> : <MdLightMode />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, i) => (
           <li key={i}>
